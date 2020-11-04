c89fe87069b214254b83061e9de63e4aeea8c1fd
Liliya Sagitova <guverboil@gmail.com>
2020-11-04 12:22:43 +0300

    Adds an index.js including imports view-classes

---
+++ src/view/index.js
@@ -0,0 +1,8 @@
+import {default as PageContainerView} from './view/container';
+import {default as ContainerView} from './view/container';
+import {default as LogoView} from './view/logo';
+import {default as CardView} from './view/cards';
+import {default as FiltersView} from './view/filters';
+import {default as FlightsView} from './view/flights';
+import {default as TabsView} from './view/tabs';
+import {default as MessageView} from './view/message';

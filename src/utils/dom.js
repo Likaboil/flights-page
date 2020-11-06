d88eaeb82421184c8fcbf9a9fe2e1272d0798f86
Liliya Sagitova <guverboil@gmail.com>
2020-11-06 18:16:42 +0300

    Adds a function getElement at dom-utils

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,6 +1,16 @@
+import {AbstractView} from '../view/index';
+
 export const createElement = (template) => {
   const element = document.createElement(`div`);
   element.innerHTML = template;
 
   return element.firstChild;
 };
+
+export const getElement = (element) => {
+  if (element instanceof AbstractView) {
+    return element.getElement();
+  }
+
+  return element;
+}

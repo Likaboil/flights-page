c72e616410acb2c045a6bbd01308d967f8383a14
Liliya Sagitova <guverboil@gmail.com>
2020-11-06 17:00:17 +0300

    Fixes a Flights-view class

--- src/view/flights/flights.js
+++ src/view/flights/flights.js
@@ -1,12 +1,16 @@
-import {Abstract} from '../../abstract';
+import {AbstractView} from '../index';
 
 const createFlightsTemplate = () => {
-  return `
-  <div class="flights">
+  return `<div class="flights">
+    <ul class="flights__list">
+      <li class="flights__card card"></li>
+      <li class="flights__card card"></li>
+      <li class="flights__card card"></li>
+      <li class="flights__card card"></li>
   </div>`;
 }
 
-export default class CreateFlights extends Abstract {
+export default class CreateFlights extends AbstractView {
   constructor() {
     super();
   }

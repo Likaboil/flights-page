74feb51846e31f1fa7fb3248fcdcbdf151560adf
Liliya Sagitova <guverboil@gmail.com>
2020-11-06 16:52:36 +0300

    Fixes a Logo-view class

--- src/view/logo/logo.js
+++ src/view/logo/logo.js
@@ -1,14 +1,14 @@
-import {Abstract} from '../abstract';
+import {AbstractView} from '../index';
+
 
 const createLogoTemplate = () => {
-  return `
-    <div class="logo">
+  return `<div class="logo">
       <img src="./img/logo-bg.svg" alt="" class="logo__bg" width="60" height="60">
       <img src="./img/plane.svg" alt="" class="logo__upper">
     </div>`;
 }
 
-export default class CreateLogo extends Abstract {
+export default class CreateLogo extends AbstractView {
   constructor() {
     super();
   }

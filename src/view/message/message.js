d53f8ac95001830478cb987af6e58bd447d3f4a0
Liliya Sagitova <guverboil@gmail.com>
2020-11-08 14:31:31 +0300

    Fixes a Message-view class

--- src/view/message/message.js
+++ src/view/message/message.js
@@ -1,17 +1,18 @@
-import {Abstract} from '../abstract';
+import {AbstractView} from '../index';
 
 const createMessageTemplate = () => {
-  return `
-    <div class="message">
-      <p class="message__title">По вашему запросу ничего не найдено!</p>
-      <p class="message__text">Попробуйте изменить запрос.</p>
-    </div>
-    <div class="message">
-      <p class="message__text">Подождите, загружаю рейсы</p>
-    </div>`;
+  return (
+  `<div class="message">
+    <p class="message__title">По вашему запросу ничего не найдено!</p>
+    <p class="message__text">Попробуйте изменить запрос.</p>
+  </div>
+  <div class="message">
+    <p class="message__text">Подождите, загружаю рейсы</p>
+  </div>`
+  );
 }
 
-export default class CreateMessage extends Abstract {
+export default class MessageView extends AbstractView {
   constructor() {
     super();
   }

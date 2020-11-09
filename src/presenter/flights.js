0adcbeae3a7ae6c01c3afeb8a49bbf178ef0c8e6
Liliya Sagitova <guverboil@gmail.com>
2020-11-09 16:01:46 +0300

    Adds a FlightsPresenter-class

---
+++ src/presenter/flights.js
@@ -0,0 +1,11 @@
+import {FlightsView, CardView, PageView} from '../view/index.js';
+
+export default class FlightsPresenter {
+  constructor(container) {
+    this._container = container;
+    this._isLoading = true;
+    this._flightsView = new FlightsView();
+    this._cardView = new CardView();
+    this._pageView = new PageView();
+  }
+}

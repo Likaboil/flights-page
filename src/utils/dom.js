47ae7814ebd89a57e2021618174e0837655c7fd5
Liliya Sagitova <guverboil@gmail.com>
2020-11-09 14:22:19 +0300

    Adds a function render and variable renderPosition at dom.js in utils

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,5 +1,12 @@
 import {AbstractView} from '../view/index';
 
+export const RenderPosition = {
+  BEFORE_BEGIN: `BEFORE_BEGIN`,
+  AFTER_BEGIN: `AFTER_BEGIN`,
+  BEFORE_END: `BEFORE_END`,
+  AFTER_END: `AFTER_END`,
+};
+
 export const createElement = (template) => {
   const element = document.createElement(`div`);
   element.innerHTML = template;
@@ -13,4 +20,26 @@ export const getElement = (element) => {
   }
 
   return element;
+}
+
+export const render = (container, element, position = RenderPosition.BEFORE_END) => {
+  container = getElement(container);
+  element = getElement(element);
+
+  switch (position) {
+    case RenderPosition.BEFORE_BEGIN:
+      container.before(element);
+      break;
+    case RenderPosition.AFTER_BEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFORE_END:
+      container.append(element);
+      break;
+    case RenderPosition.AFTER_END:
+      container.after(element);
+      break;
+    default:
+      throw new Error(`Unknown render position: ${position}`);
+  }
 }

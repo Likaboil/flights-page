573a46ef67259720216553b8e07f888297a79209
Liliya Sagitova <guverboil@gmail.com>
2020-11-11 11:29:17 +0300

    Splits the view-template into more view-classes at Flights-view

--- src/view/flights/flights.js
+++ src/view/flights/flights.js
@@ -3,14 +3,28 @@ import {AbstractView} from '../index';
 const createFlightsTemplate = () => {
   return (
     `<div class="flights">
-      <ul class="flights__list">
-        <li class="flights__card card"></li>
-        <li class="flights__card card"></li>
-        <li class="flights__card card"></li>
-        <li class="flights__card card"></li>
-      </ul>
     </div>`
     );
+}
+
+const createFlightsList = () => {
+  return (`<ul class="flights__list"></ul>`);
+}
+
+const createFlightsItem = () => {
+  return (`<li class="flights__card card"></li>`);
+}
+
+class FlightsListView extends AbstractView {
+  getTemplate() {
+    return createFlightsList();
+  }
+}
+
+class FlightsItemView extends AbstractView {
+  getTemplate() {
+    return createFlightsItem();
+  }
 }
 
 export default class FlightsView extends AbstractView {

394d0154f476419bcd7eb529121a09f54aa7b8b9
Liliya Sagitova <guverboil@gmail.com>
2020-11-11 11:32:57 +0300

    Adds a FlightsListView, FlightsItemView at export in view/index.js

--- src/view/index.js
+++ src/view/index.js
@@ -4,6 +4,6 @@ export {default as ContainerView} from './container/container';
 export {default as LogoView} from './logo/logo';
 export {default as CardView} from './cards/cards';
 export {default as FiltersView} from './filters/filters';
-export {default as FlightsView} from './flights/flights';
+export {default as FlightsView, FlightsListView, FlightsItemView} from './flights/flights';
 export {default as TabsView} from './tabs/tabs';
 export {default as MessageView} from './message/message';

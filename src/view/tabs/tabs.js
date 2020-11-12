a397089b4dcef3ba8e148eecfe9eebc0df20fe8d
Liliya Sagitova <guverboil@gmail.com>
2020-11-12 20:25:51 +0300

    Fixes missing semicolons, spare constructor in TabsView-classsrc/view/tabs/tabs.js

--- src/view/tabs/tabs.js
+++ src/view/tabs/tabs.js
@@ -2,24 +2,21 @@ import {AbstractView} from '../index';
 
 const createTabsTemplate = () => {
   return (
-  `<div>
-    <h2 class="visually-hidden">Фильтрация по скорости перелета</h2>
-    <ul class="tabs">
-      <li class="tabs__item tabs__item--cheaper tabs__item--active">
-        <a class="tabs__text tabs__text--active"> Самый дешевый</a>
-      </li>
-      <li class="tabs__item tabs__item--faster">
-        <a class="tabs__text"> Самый быстрый</a>
-      </li>
-    </ul
-  </div>`
+    `<div>
+      <h2 class="visually-hidden">Фильтрация по скорости перелета</h2>
+      <ul class="tabs">
+        <li class="tabs__item tabs__item--cheaper tabs__item--active">
+          <a class="tabs__text tabs__text--active"> Самый дешевый</a>
+        </li>
+        <li class="tabs__item tabs__item--faster">
+          <a class="tabs__text"> Самый быстрый</a>
+        </li>
+      </ul
+    </div>`
   );
-}
+};
 
 export default class TabsView extends AbstractView {
-  constructor() {
-    super();
-  }
 
   getTemplate() {
     return createTabsTemplate();

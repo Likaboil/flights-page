84df6cddffb4c77ee0b24978976c78e76affcbc5
Liliya Sagitova <guverboil@gmail.com>
2020-11-12 20:13:31 +0300

    Fixes a template, missing semicolons, spare constructor in ContainerView-class

--- src/view/container/container.js
+++ src/view/container/container.js
@@ -1,15 +1,10 @@
 import {AbstractView} from '../index';
 
 const createContainerTemplate = () => {
-  return (
-  `<div class="container"></div>`
-  );
-}
+  return `<div class="container"></div>`;
+};
 
 export default class ContainerView extends AbstractView {
-  constructor() {
-    super();
-  }
 
   getTemplate() {
     return createContainerTemplate();

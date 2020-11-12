654d226be2367fd30634bf07e0e573312e7dcfd9
Liliya Sagitova <guverboil@gmail.com>
2020-11-12 20:20:05 +0300

    Fixes templates, missing semicolons, spare constructor in FlightsView-class

--- src/view/flights/flights.js
+++ src/view/flights/flights.js
@@ -1,19 +1,16 @@
 import {AbstractView} from '../index';
 
 const createFlightsTemplate = () => {
-  return (
-    `<div class="flights">
-    </div>`
-    );
-}
+  return `<div class="flights"></div>`;
+};
 
 const createFlightsList = () => {
-  return (`<ul class="flights__list"></ul>`);
-}
+  return `<ul class="flights__list"></ul>`;
+};
 
 const createFlightsItem = () => {
-  return (`<li class="flights__card card"></li>`);
-}
+  return `<li class="flights__card card"></li>`;
+};
 
 class FlightsListView extends AbstractView {
   getTemplate() {
@@ -28,9 +25,6 @@ class FlightsItemView extends AbstractView {
 }
 
 export default class FlightsView extends AbstractView {
-  constructor() {
-    super();
-  }
 
   getTemplate() {
     return createFlightsTemplate();

2efbd6c2eefcb747c922b828e208955a71322e56
Liliya Sagitova <guverboil@gmail.com>
2020-11-18 18:34:13 +0300

    Adds the function getFormatDateToHHMM in date.js at utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,5 @@
 import Moment from 'moment';
 import {extendMoment} from 'moment-range';
 const moment = extendMoment(Moment);
+
+export const getFormatDateToHHMM = (date) => moment(date).format(`HH:mm`);

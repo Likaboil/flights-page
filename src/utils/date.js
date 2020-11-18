f05a23431ab9e1abf8576bfd40858d2ad733b8e3
Liliya Sagitova <guverboil@gmail.com>
2020-11-18 18:38:31 +0300

    Adds the function getFlightDurationInMs in date.js at utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,3 +5,11 @@ const moment = extendMoment(Moment);
 export const getFormatDateToHHMM = (date) => moment(date).format(`HH:mm`);
 
 export const concatinateTimes = (startDate, endDate) => `${getFormatDateToHHMM(startDate)}-${getFormatDateToHHMM(endDate)}`;
+
+export const getFlightDurationInMs = (startDate, endDate) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const range = moment.range(start, end);
+  return range.duration(`milliseconds`);
+};

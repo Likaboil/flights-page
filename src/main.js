55953833f4bd7b2a794ae7b2cd29efef2621f41c
Liliya Sagitova <guverboil@gmail.com>
2020-11-18 11:16:26 +0300

    Adds const FLIGHTS_MODEL to main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {default as Page} from './presenter/page.js';
 import {FLIGHTS} from './mocks/flights';
 
 const rootElement = document.querySelector(`#root`);
+const FLIGHTS_MODEL = FLIGHTS;
 const newPage = new Page(rootElement);
 
 newPage.init();

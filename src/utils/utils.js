665f903410737dc0bb3143dc7c7c8abb355ff6cd
Liliya Sagitova <guverboil@gmail.com>
2020-11-19 11:37:13 +0300

    Adds import const ENDING_OF_TRANSFER_AMOUNT_LABEL in utils.js at utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import {ENDING_OF_TRANSFER_AMOUNT_LABEL} from '../const';
+
 export const splitPrice = (price) => {
   const priceString = price.toString();
   return `${priceString.slice(0, 2)} ${priceString.slice(2, 5)} ${priceString.slice(5, 8)}`;

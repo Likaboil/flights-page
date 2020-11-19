2b07d7ab1a066b207aa5f92d5324832dcbf48b34
Liliya Sagitova <guverboil@gmail.com>
2020-11-19 16:17:48 +0300

    Rename PagePresenter-class at main.js. Code Review

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import 'normalize.css';
 import './style.scss';
 
-import {default as Page} from './presenter/page.js';
+import {default as PagePresenter} from './presenter/page.js';
 import {FLIGHTS} from './mocks/flights';
 
 const rootElement = document.querySelector(`#root`);
 const FLIGHTS_MODEL = FLIGHTS;
-const newPage = new Page(rootElement, FLIGHTS_MODEL);
 
+const newPage = new PagePresenter(rootElement, FLIGHTS_MODEL);
 newPage.init();

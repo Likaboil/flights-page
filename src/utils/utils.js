4bd97a2be196343b94f90270b1b8f9e9de94ae1e
Liliya Sagitova <guverboil@gmail.com>
2020-11-19 11:22:04 +0300

    Adds the function getTransferAmount in utils.js at utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,3 +4,5 @@ export const splitPrice = (price) => {
 };
 
 export const getTransferPoints = (transferPoint) => transferPoint.join(`, `);
+
+export const getTransferAmount = (transferPoint) => transferPoint.length;

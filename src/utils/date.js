bdf0923c1df5b4486c617aa5d3a52cfe4c6f0945
Liliya Sagitova <guverboil@gmail.com>
2020-11-19 10:25:49 +0300

    Adds the function getFlightDurationLabel in date.js at utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -14,3 +14,14 @@ export const getFormatDurationToDHM = (ms) => {
     minutes: duration.minutes(ms),
   };
 };
+
+export const getFlightDurationLabel = (startDate, endDate) => {
+  const flightTimeInMs = getFlightDurationInMs(startDate, endDate);
+  const flightTime = getFormatDurationToDHM(flightTimeInMs);
+
+  if (flightTime.days > 0) {
+    return `${flightTime.days}д ${flightTime.hours}ч ${flightTime.minutes}м`;
+  }
+
+  return `${flightTime.hours}ч ${flightTime.minutes}м`;
+};

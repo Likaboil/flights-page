45b45ccb6d17d743c927ddba5b03f2276279a256
Liliya Sagitova <guverboil@gmail.com>
2020-11-19 10:17:24 +0300

    Adds the function getFormatDurationToDHM in date.js at utils

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,3 +5,12 @@ const moment = extendMoment(Moment);
 export const getFormatDateToHHMM = (date) => moment(date).format(`HH:mm`);
 
 export const getFlightDurationInMs = (startDate, endDate) => moment.range(startDate, endDate).duration(`milliseconds`);
+
+export const getFormatDurationToDHM = (ms) => {
+  const duration = moment.duration(ms);
+  return {
+    days: duration.days(ms),
+    hours: duration.hours(ms),
+    minutes: duration.minutes(ms),
+  };
+};

f46b4749b3563447e4cb6b607cc8cd34cdcc45b1
Liliya Sagitova <guverboil@gmail.com>
2020-11-19 11:20:46 +0300

    Adds the function getTransferPoints in utils.js at utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,3 +2,5 @@ export const splitPrice = (price) => {
   const priceString = price.toString();
   return `${priceString.slice(0, 2)} ${priceString.slice(2, 5)} ${priceString.slice(5, 8)}`;
 };
+
+export const getTransferPoints = (transferPoint) => transferPoint.join(`, `);

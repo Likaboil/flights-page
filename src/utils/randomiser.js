aa32d9b6bca4bdababc56a466ebfe4356b7a4dc3
Liliya Sagitova <guverboil@gmail.com>
2020-11-23 13:56:09 +0300

    Adds import consts MINUTE, HOUR, DAY to randomiser.js

--- src/utils/randomiser.js
+++ src/utils/randomiser.js
@@ -1,3 +1,5 @@
+import {MINUTE, HOUR, DAY} from '../const';
+
 export const getRandomInt = (min, max) => min + Math.floor(Math.random() * (max - min));
 export const getRandomArrayValue = (arr) => arr[Math.floor(Math.random() * arr.length)];
 export const getRandomArray = (arr, length = arr.length) => arr

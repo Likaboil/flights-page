7da9f8e912164f37bcceac2a232ce4b61f0ff313
Liliya Sagitova <guverboil@gmail.com>
2020-11-23 13:57:04 +0300

    Adds function getRandomDate to randomiser.js

--- src/utils/randomiser.js
+++ src/utils/randomiser.js
@@ -6,3 +6,13 @@ export const getRandomArray = (arr, length = arr.length) => arr
     .slice()
     .sort(() => Math.random() - 0.5)
     .slice(0, length);
+
+export const getRandomDate = ({date = new Date(), minutes = 59, hours = 24, days = 1}) => {
+  const minutesMs = getRandomInt(0, minutes * MINUTE);
+  const hoursMs = getRandomInt(0, hours * HOUR);
+  const daysMs = getRandomInt(0, days * DAY);
+
+  const randomDateMs = +date + minutesMs + hoursMs + daysMs;
+
+  return new Date(randomDateMs);
+};

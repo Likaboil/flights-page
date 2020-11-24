6b254d815aa04387dbb64804837b3e0f1626d6dc
Liliya Sagitova <guverboil@gmail.com>
2020-11-24 11:54:45 +0300

    Adds import of generateFlights and adaptFlightsToClient to main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './style.scss';
 
 import {default as PagePresenter} from './presenter/page.js';
 import {FLIGHTS} from './mocks/flights';
+import {generateFlights} from './mocks/flights-generator';
+import {adaptFlightsToClient} from './adapter/flights';
+
 
 const rootElement = document.querySelector(`#root`);
 const FLIGHTS_MODEL = FLIGHTS;
+
 
 const newPage = new PagePresenter(rootElement, FLIGHTS_MODEL);
 newPage.init();

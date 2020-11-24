4541d038205b4db6e916f7a0fa047b9a3031a340
Liliya Sagitova <guverboil@gmail.com>
2020-11-24 11:57:27 +0300

    Removes an import const FLIGHTS from mocks at main.js

--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@ import 'normalize.css';
 import './style.scss';
 
 import {default as PagePresenter} from './presenter/page.js';
-import {FLIGHTS} from './mocks/flights';
 import {generateFlights} from './mocks/flights-generator';
 import {adaptFlightsToClient} from './adapter/flights';
 
-
 const rootElement = document.querySelector(`#root`);
 const FLIGHTS_MODEL = FLIGHTS;
-
 
 const newPage = new PagePresenter(rootElement, FLIGHTS_MODEL);
 newPage.init();

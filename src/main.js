b14154bdc2879359bbd2e5bdb6357f803b1514e0
Liliya Sagitova <guverboil@gmail.com>
2020-11-24 11:59:14 +0300

    Adds const NEW_FLIGHTS at main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import {generateFlights} from './mocks/flights-generator';
 import {adaptFlightsToClient} from './adapter/flights';
 
 const rootElement = document.querySelector(`#root`);
+
+const NEW_FLIGHTS = generateFlights(5);
+
 const FLIGHTS_MODEL = FLIGHTS;
 
 const newPage = new PagePresenter(rootElement, FLIGHTS_MODEL);

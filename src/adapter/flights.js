b38c1e8bb39d028963e42f76cc5694af9b959f07
Liliya Sagitova <guverboil@gmail.com>
2020-11-24 11:49:40 +0300

    Adds adapter of flights, function adaptFlight to src/adapter

---
+++ src/adapter/flights.js
@@ -0,0 +1,23 @@
+export const adaptFlight = (newFlight) => {
+
+  return {
+    id: newFlight.id,
+    airCompanyName: newFlight.airCompany.name,
+    airCompanyLogo: newFlight.airCompany.logo,
+    price: Number(newFlight.price),
+    directFlight: {
+      departurePoint: newFlight.firstPoint,
+      departureTime: newFlight.firstStartDate,
+      arrivalPoint: newFlight.secondPoint,
+      arrivalTime: newFlight.firstEndDate,
+      transferPoints: newFlight.firstTransferPoints,
+    },
+    returnFlight: {
+      departurePoint: newFlight.secondPoint,
+      departureTime: newFlight.secondStartDate,
+      arrivalPoint: newFlight.firstPoint,
+      arrivalTime: newFlight.secondEndDate,
+      transferPoints: newFlight.secondTransferPoints,
+    },
+  };
+};

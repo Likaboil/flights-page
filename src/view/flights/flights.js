a5c390b473ecd8a56f92f73485716a48868790d3
Liliya Sagitova <guverboil@gmail.com>
2020-11-25 19:03:10 +0300

    Replace FlightListView-class and FlightsItemView-class from flights.js to a separate flights-list.js, fixes exports in index.js. Code Review

--- src/view/flights/flights.js
+++ src/view/flights/flights.js
@@ -4,33 +4,9 @@ const createFlightsTemplate = () => {
   return `<div class="flights"></div>`;
 };
 
-const createFlightsList = () => {
-  return `<ul class="flights__list"></ul>`;
-};
-
-const createFlightsItem = () => {
-  return `<li class="flights__card card"></li>`;
-};
-
-class FlightsListView extends AbstractView {
-  getTemplate() {
-    return createFlightsList();
-  }
-}
-
-class FlightsItemView extends AbstractView {
-  getTemplate() {
-    return createFlightsItem();
-  }
-}
-
 export default class FlightsView extends AbstractView {
 
   getTemplate() {
     return createFlightsTemplate();
   }
 }
-
-export {
-  FlightsListView, FlightsItemView
-};

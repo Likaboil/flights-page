42cffb85f87b5587abbf33ac20367ebc23d04bd1
Liliya Sagitova <guverboil@gmail.com>
2020-11-25 19:42:21 +0300

    Adds util flights-generator at mocks, adds consts FLIGHTS_POINTS, TRANSFER_POINTS, AIR_COMPANY in flights-generator

---
+++ src/mocks/flights-generator.js
@@ -0,0 +1,17 @@
+const FLIGHTS_POINTS = {
+  FIRST_POINT: `MOW`,
+  SECOND_POINT: `HKT`,
+};
+
+const TRANSFER_POINTS = [`BKK`, `HKG`, `DOH`, `IST`, `JNB`];
+
+const AIR_COMPANY = [
+  {
+    NAME: `Turkish Airlines`,
+    LOGO: `turkish-logo.png`,
+  },
+  {
+    NAME: `S7`,
+    LOGO: `s7logo.png`,
+  }
+];

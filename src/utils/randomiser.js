986f28de92caf470c6ff98bac7149717ca8e6075
Liliya Sagitova <guverboil@gmail.com>
2020-11-25 19:34:35 +0300

    Adds function getRandomArray to randomiser-util

--- src/utils/randomiser.js
+++ src/utils/randomiser.js
@@ -1,2 +1,6 @@
 export const getRandomInt = (min, max) => min + Math.floor(Math.random() * (max - min));
 export const getRandomArrayValue = (arr) => arr[Math.floor(Math.random() * arr.length)];
+export const getRandomArray = (arr, length = arr.length) => arr
+    .slice()
+    .sort(() => Math.random() - 0.5)
+    .slice(0, length);

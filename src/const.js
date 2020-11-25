3a572a0de4830b044fa0d240a143aacba8497606
Liliya Sagitova <guverboil@gmail.com>
2020-11-25 19:35:19 +0300

    Adds const MINUTE, HOUR, DAY at const.js

--- src/const.js
+++ src/const.js
@@ -7,3 +7,7 @@ export const MESSAGE = {
 
 export const NO_TRANSFER_AMOUNT_LABEL = `Без пересадок`;
 export const ENDING_OF_TRANSFER_AMOUNT_LABEL = [`пересадка`, `пересадки`, `пересадок`];
+
+export const MINUTE = 60 * 1000;
+export const HOUR = 60 * MINUTE;
+export const DAY = 24 * HOUR;

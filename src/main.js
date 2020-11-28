b825a22eac4ff87323506fb0ad6706726e2140d6
Liliya Sagitova <guverboil@gmail.com>
2020-11-28 13:33:48 +0300

    Adds import of const Filters and its usage as FILTERS_MODEL in pagePresenter at main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import './style.scss';
 import {default as PagePresenter} from './presenter/page.js';
 import {generateFlights} from './mocks/flights-generator';
 import {adaptFlightsToClient} from './adapter/flights';
+import {FILTERS} from './const';
 
 const rootElement = document.querySelector(`#root`);
 const FLIGHTS_MODEL = adaptFlightsToClient(generateFlights(5));
-
-const pagePresenter = new PagePresenter(rootElement, FLIGHTS_MODEL);
+const FILTERS_MODEL = FILTERS;
+const pagePresenter = new PagePresenter(rootElement, FLIGHTS_MODEL, FILTERS_MODEL);
 pagePresenter.init();

335da98c9f7570d9df07afb3bc9470927424e601
Liliya Sagitova <guverboil@gmail.com>
2020-11-30 14:30:06 +0300

    Adds import and usage of FlightsModel in PagePresenter at main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import {default as PagePresenter} from './presenter/page.js';
 import {generateFlights} from './mocks/flights-generator';
 import {adaptFlightsToClient} from './adapter/flights';
 import {FILTERS, SORTING_FILTERS} from './const';
+import {default as FlightsModel} from './model/flights';
 
 const rootElement = document.querySelector(`#root`);
 const FLIGHTS_MODEL = adaptFlightsToClient(generateFlights(5));
 const FILTERS_MODEL = FILTERS;
 const SORT_MODEL = SORTING_FILTERS;
-const pagePresenter = new PagePresenter(rootElement, FLIGHTS_MODEL, FILTERS_MODEL, SORT_MODEL);
+
+const flightsModel = new FlightsModel();
+flightsModel.set(FLIGHTS_MODEL);
+
+const pagePresenter = new PagePresenter(rootElement, flightsModel, FILTERS_MODEL, SORT_MODEL);
 pagePresenter.init();

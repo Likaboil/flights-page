3864030a9ccd133dbc8158f7011e73375fd55ef9
Liliya Sagitova <guverboil@gmail.com>
2020-11-30 13:56:09 +0300

    Adds import const SORTING_FILTERS and it's usage in PagePresenter as SORT_MODEL  at main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import './style.scss';
 import {default as PagePresenter} from './presenter/page.js';
 import {generateFlights} from './mocks/flights-generator';
 import {adaptFlightsToClient} from './adapter/flights';
-import {FILTERS} from './const';
+import {FILTERS, SORTING_FILTERS} from './const';
 
 const rootElement = document.querySelector(`#root`);
 const FLIGHTS_MODEL = adaptFlightsToClient(generateFlights(5));
 const FILTERS_MODEL = FILTERS;
-const pagePresenter = new PagePresenter(rootElement, FLIGHTS_MODEL, FILTERS_MODEL);
+const SORT_MODEL = SORTING_FILTERS;
+const pagePresenter = new PagePresenter(rootElement, FLIGHTS_MODEL, FILTERS_MODEL, SORT_MODEL);
 pagePresenter.init();

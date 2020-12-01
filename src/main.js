9f04426b756926891291560a0702b56b3630c5b5
Liliya Sagitova <guverboil@gmail.com>
2020-12-01 16:50:32 +0300

    Renames folder model to models and in all files with it's usage. Code Review

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import {default as PagePresenter} from './presenters/page.js';
 import {generateFlights} from './mocks/flights-generator';
 import {adaptFlightsToClient} from './adapter/flights';
 import {FILTERS, SORTING_FILTERS} from './const';
-import {default as FlightsModel} from './model/flights';
+import {default as FlightsModel} from './models/flights';
 
 const rootElement = document.querySelector(`#root`);
 const FLIGHTS_MODEL = adaptFlightsToClient(generateFlights(5));

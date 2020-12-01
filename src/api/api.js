143e2a202fee5a59f600699ee58bc9f3bd4cf72c
Liliya Sagitova <guverboil@gmail.com>
2020-12-01 18:08:12 +0300

    Adds class Api to api.js

--- src/api/api.js
+++ src/api/api.js
@@ -6,3 +6,10 @@ const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export const FLIGHT_AMOUNT = 5;
 export const mockFlights = generateFlights(FLIGHT_AMOUNT);
+
+export default class Api {
+
+  static getFlights() {
+    return delay(DELAY_MS).then(() => adaptFlightsToClient(mockFlights));
+  }
+}

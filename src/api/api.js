f8e408c8f3e9b408fb12b86317ebbbd680e9b5a9
Liliya Sagitova <guverboil@gmail.com>
2020-12-01 17:14:41 +0300

    Adds const DELAY_MS and function delay  to Api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,5 @@
 import {generateFlights} from './mocks/flights-generator';
 import {adaptFlightsToClient} from './adapter/flights';
+
+const DELAY_MS = 500;
+const delay = (ms) => new Promise((res) => setTimeout(res, ms) )

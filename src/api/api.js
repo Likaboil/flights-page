7bef8a38c9cbd5f47bc8862194432e9f3aaf8d50
Liliya Sagitova <guverboil@gmail.com>
2020-12-01 18:07:10 +0300

    Adds const FLIGHT_AMOUNTand mockFlights to Api

--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,6 @@ import {adaptFlightsToClient} from '../adapter/flights';
 
 const DELAY_MS = 500;
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+export const FLIGHT_AMOUNT = 5;
+export const mockFlights = generateFlights(FLIGHT_AMOUNT);

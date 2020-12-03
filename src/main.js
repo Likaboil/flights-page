6d4fcb0fdbbf3316f0790d3784ddbe1a9be8ec3b
Liliya Sagitova <guverboil@gmail.com>
2020-12-03 16:38:29 +0300

    WIP. Remove generating flights, imports of functions to generate flights from flightsModel in main.js

--- src/main.js
+++ src/main.js
@@ -2,19 +2,14 @@ import 'normalize.css';
 import './style.scss';
 
 import {default as PagePresenter} from './presenters/page.js';
-import {generateFlights} from './mocks/flights-generator';
-import {adaptFlightsToClient} from './adapter/flights';
 import {FILTERS, SORTING_FILTERS, UpdateType} from './const';
 import {default as FlightsModel} from './models/flights';
 import {default as Api} from './api/api';
 
 const rootElement = document.querySelector(`#root`);
-const FLIGHTS_MODEL = adaptFlightsToClient(generateFlights(5));
 const FILTERS_MODEL = FILTERS;
 const SORT_MODEL = SORTING_FILTERS;
-
 const flightsModel = new FlightsModel();
-flightsModel.set(FLIGHTS_MODEL);
 
 const pagePresenter = new PagePresenter(rootElement, flightsModel, FILTERS_MODEL, SORT_MODEL);
 pagePresenter.init();

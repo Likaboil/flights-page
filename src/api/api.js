a85fbde62bc67ef12c19137e2b3b6ee6c0e6a249
Liliya Sagitova <guverboil@gmail.com>
2020-12-03 18:28:02 +0300

    WIP. Increases DELAY_MS at api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import {generateFlights} from '../mocks/flights-generator';
 import {adaptFlightsToClient} from '../adapter/flights';
 
-const DELAY_MS = 500;
+const DELAY_MS = 2000;
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export const FLIGHT_AMOUNT = 5;

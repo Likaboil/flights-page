6716324b0ca49024b297ac86a6728f1a9ad7fb5e
Liliya Sagitova <guverboil@gmail.com>
2020-12-04 10:19:34 +0300

    WIP. Adds catch errors to generation flightsModel, adds import ServerErrorMessage with it's usage at main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'normalize.css';
 import './style.scss';
 
 import {default as PagePresenter} from './presenters/page.js';
-import {FILTERS, SORTING_FILTERS, UpdateType} from './const';
+import {FILTERS, SORTING_FILTERS, UpdateType, ServerErrorMessage} from './const';
 import {default as FlightsModel} from './models/flights';
 import {default as Api} from './api/api';
 
@@ -11,9 +11,13 @@ const FILTERS_MODEL = FILTERS;
 const SORT_MODEL = SORTING_FILTERS;
 const flightsModel = new FlightsModel();
 
+
 Api.getFlights()
   .then((flights) => {
     flightsModel.set(UpdateType.INIT, flights);
+  })
+  .catch(() => {
+    flightsModel.setError(UpdateType.ERROR, ServerErrorMessage);
   });
 
 const pagePresenter = new PagePresenter(rootElement, flightsModel, FILTERS_MODEL, SORT_MODEL);
